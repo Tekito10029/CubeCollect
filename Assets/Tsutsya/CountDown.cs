900f6257ba1b3a849e43e11aa8ad1da547d0942f
kei0527 <t.kei0527@gmail.com>
2022-07-02 03:03:32 +0900

    タイマーカウントの実装

---
+++ Assets/Tsutsya/CountDown.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+	public static float CountDownTime;  // カウントダウンタイム
+	public Text TextCountDown;              // 表示用テキストUI
+
+	// Use this for initialization
+	void Start()
+	{
+		CountDownTime = 60.0F;  // カウントダウン開始秒数をセット
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// カウントダウンタイムを整形して表示
+		TextCountDown.text = String.Format("Time: {0:00.00}", CountDownTime);
+		// 経過時刻を引いていく
+		CountDownTime -= Time.deltaTime;
+		// 0.0秒以下になったらカウントダウンタイムを0.0で固定（止まったように見せる）
+		if (CountDownTime <= 0.0F)
+		{
+			CountDownTime = 0.0F;
+		}
+	}
+}
